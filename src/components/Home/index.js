94891b1bbc8e3ab88cc47b741507a1246909b8f6
Shwetha <14ss200@caslab.queensu.ca>
2017-10-22 01:09:16 -0400

    test

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 import homeStyles from '../../styles/home';
+//import ReactDOM from 'react-dom';
+import Rectangle from 'react-rectangle';
 
 export default class Home extends Component {
     constructor(props) {
@@ -12,6 +14,11 @@ export default class Home extends Component {
         return (
             <View style={homeStyles.container}>
                 <Text>This is the start of the home page</Text>
+                <View style={{ background: '#9e9e9e', width: '100vw', height: '100vh' }}>
+                <Rectangle aspectRatio={[5, 3]}>
+                <View style={{ background: '#607d8b', width: '100%', height: '100%' }} />
+                </Rectangle>
+              </View>,
             </View>
         );
     }
